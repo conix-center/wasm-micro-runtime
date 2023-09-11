c193e5d978e4ac8a555d6a00686cd2aa3fc68263
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2023-09-11 23:24:54 +0530

    Fix post-rebase changes (yet to fix proc_exit with infinite suspend child thread)

--- core/iwasm/aot/aot_runtime.c
+++ core/iwasm/aot/aot_runtime.c
@@ -43,7 +43,7 @@ bh_static_assert(offsetof(AOTModuleInstance, cur_exception)
 bh_static_assert(offsetof(AOTModuleInstance, global_table_data)
                  == 13 * sizeof(uint64) + 128 + 11 * sizeof(uint64));
 
-bh_static_assert(sizeof(AOTMemoryInstance) == 104);
+bh_static_assert(sizeof(AOTMemoryInstance) == 112);
 bh_static_assert(offsetof(AOTTableInstance, elems) == 8);
 
 bh_static_assert(offsetof(AOTModuleInstanceExtra, stack_sizes) == 0);
