6978c06a70ca1a0cd6a37923b70e9283e24e67a6
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2024-06-11 08:57:58 -0400

    Added verbose flag for zephyr build

--- product-mini/platforms/zephyr/simple/build_nucleo.sh
+++ product-mini/platforms/zephyr/simple/build_nucleo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+vb="${1:-2}"
+
 west build -b nucleo_l433rc_p . -p always -- \
            -DWAMR_BUILD_TARGET=THUMBV7 -DWAMR_BUILD_AOT=0 \
-           -DWAMR_BUILD_LIBC_WAZI=1 -DLOG_VERBOSITY=$1
+           -DWAMR_BUILD_LIBC_WAZI=1 -DLOG_VERBOSITY=$vb
 west flash
