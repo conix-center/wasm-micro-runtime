290502e30841774465a7eb803ff7f554b543667b
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2024-06-12 12:11:08 -0400

    Initial functional autogenerated zephyr implementation

--- core/iwasm/libraries/libc-wazi/wazi_conversions.h
+++ core/iwasm/libraries/libc-wazi/wazi_conversions.h
@@ -10,4 +10,17 @@
   no_convert ? (FILE*) wasm_ptr : (FILE*) MADDR(wasm_ptr); \
 })
 
+/* Assigning to a log_msg_desc 32-bit bitfield struct */
+#define LOG_MSG_DESC_INIT(v) ({ \
+  struct log_msg_desc res;  \
+  memcpy(&res, &v, sizeof(uint32_t)); \
+  res; \
+})
+
+/* Assigning to a va_list: Not supported currently */
+#define VA_LIST_INIT(v) ({ \
+  va_list c;  \
+  c; \
+})
+
 #endif
